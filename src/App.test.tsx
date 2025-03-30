9410b2737febc8e87762a7320d31127b24f69ced
Taru Airola <taru.airola@wunderdog.fi>
2025-03-30 20:36:13 +0300

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/music quiz/i)
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/music quiz/i)
   expect(linkElement).toBeInTheDocument()
 })
